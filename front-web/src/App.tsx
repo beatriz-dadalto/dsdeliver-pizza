9dbf9b66472171b434f94186d64feb558f3c333e
Bia Coelho <biacoelho@pm.me>
2021-01-09 02:21:08 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './styles.css';
+import { ReactComponent as MainImage } from './main.svg';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <div className="home-actions">
+          <h1 className="home-title">
+            Faça seu pedido <br /> que entregamos <br /> pra você!!!
+          </h1>
+          <h3 className="home-sub-title">
+            Escolha o seu pedido e em poucos minutos <br /> levaremoss na sua
+            porta
+          </h3>
+          <a href="#link" className="home-btn-order">
+            FAZER PEDIDO
+          </a>
+        </div>
+        <div className="home-image">
+          <MainImage />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
